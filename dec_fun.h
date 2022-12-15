d3f03f79b3c5b3e5817b5e54d5dc54d7a1bcae00
Pabloo-ss <pablosouto6@gmail.com>
2022-12-15 13:40:24 +0100

    funciones no muy gucci

--- dec_fun.h
+++ dec_fun.h
@@ -1,12 +1,48 @@
-int funcionA (int ee, int rer){
-float funcionDentro (float aa){
-return aa;
+float max (float num1, float num2){
+float maximo;
+int temp1;
+temp1 = num1 >= num2; 
+if (!temp1) goto etiqueta1;
+{
+	
+{
+	maximo = num1;
 }
-return 32;
 }
-float funcionR (int e2){
-float temp4;
-temp4 = 4.6 + 3.0; 
-float r1 = temp4;
-return r1;
+goto etiqueta2;
+
+etiqueta1:
+{
+	
+{
+	maximo = num2;
+}
+}
+
+etiqueta2: {} 
+float min (float numero1, float numero2){
+float minimo;
+int temp2;
+temp2 = num1 <= num2; 
+if (!temp2) goto etiqueta3;
+{
+	
+{
+	minimo = num1;
+}
+}
+goto etiqueta4;
+
+etiqueta3:
+{
+	
+{
+	minimo = num2;
+}
+}
+
+etiqueta4: {} 
+return minimo;
+}
+return maximo;
 }
