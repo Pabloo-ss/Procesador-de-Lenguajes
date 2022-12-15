4b2bb82e1bd5894c6056ddc4eb5835354cad1310
Pabloo-ss <pablosouto6@gmail.com>
2022-12-15 13:56:46 +0100

    funciones finisimas

--- dec_fun.h
+++ dec_fun.h
@@ -3,7 +3,7 @@ float maximo, minrel;
 float min (float numero1, float numero2){
 float minimo;
 int temp1;
-temp1 = num1 <= num2; 
+temp1 = numero1 <= numero2; 
 if (!temp1) goto etiqueta1;
 {
 	
@@ -29,7 +29,7 @@ return minimo;
 minrel = min(num1, num2);
 }
 int temp2;
-temp2 = num1 >= minrel; 
+temp2 = num1 > minrel; 
 if (!temp2) goto etiqueta3;
 {
 
